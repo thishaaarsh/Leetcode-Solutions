cbef85e6edf58397d1b184716bd922ad9de76fbf
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-05-04 09:59:57 +0530

    Time: 517 ms (14.48%), Space: 94.9 MB (43.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -18,7 +18,28 @@ public:
             sum+=arr[i];
         }
         if(sum%2) return false;
-        vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
-        return solve(n-1,sum/2,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(sum/2+1,0));
+        //return solve(n-1,sum/2,arr,dp);
+        for(int i=0; i<n;i++){
+            for(int j=0; j<=sum/2;j++){
+                if(j==0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if(i==0){
+                    if(arr[i] == j){
+                        dp[i][j] = 1;
+                    }
+                    continue;
+                }
+                int nottake = dp[i-1][j];
+                int take = 0;
+                if(arr[i] <= j){
+                    take = dp[i-1][j-arr[i]];
+                }
+                dp[i][j] = take || nottake;
+            }
+        }
+        return dp[n-1][sum/2];
     }
 };
