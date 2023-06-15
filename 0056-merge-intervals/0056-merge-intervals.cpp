5d58ea15431eafe33524ab76f5139b2b8dfc70b0
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-06-15 08:30:57 +0530

    Time: 44 ms (53.49%), Space: 19.3 MB (34.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,12 +5,19 @@ public:
         int n = arr.size();
         sort(arr.begin(),arr.end());
         for(int i=0; i<n; i++){
-            if(ans.size() == 0 || ans.back()[1] < arr[i][0]){
-                ans.push_back(arr[i]);
+            int start = arr[i][0];
+            int end = arr[i][1];
+            if(!ans.empty() and ans.back()[1] >= end){
+                continue;
             }
-            else{
-                ans.back()[1] = max(ans.back()[1], arr[i][1]);
+            for(int j=i+1; j<n; j++){
+                if(arr[j][0] <= end){
+                    end = max(end,arr[j][1]);
+                }
+                else break;
             }
+            vector<int>temp = {start,end};
+            ans.push_back(temp);
         }
         return ans;
     }
