7bb6446c5c43540a8490724aad12586a1d53ff26
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-06-30 18:31:38 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (52.45%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        if (matrix.empty() || matrix[0].empty()) {
-            return result;
-        }
+    vector<int> spiralOrder(vector<vector<int>>& arr) {
+        vector<int>ans;
+        int n = arr.size();
+        int m = arr[0].size();
+        if(n==0 || m== 0) return ans;
         
-        int rows = matrix.size(), cols = matrix[0].size();
-        int left = 0, right = cols-1, top = 0, bottom = rows-1;
-        
-        while (left <= right && top <= bottom) {
-            for (int i = left; i <= right; i++) {
-                result.push_back(matrix[top][i]);
+        int left = 0, right = m-1, top = 0, bottom = n-1;
+        while(left<=right and top<=bottom){
+            for(int i=left; i<=right; i++){
+                ans.push_back(arr[top][i]);
             }
             top++;
-            
-            for (int i = top; i <= bottom; i++) {
-                result.push_back(matrix[i][right]);
+            for(int i=top; i<=bottom; i++){
+                ans.push_back(arr[i][right]);
             }
             right--;
-            
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    result.push_back(matrix[bottom][i]);
-                }
+            if(top<=bottom){
+            for(int i=right; i>=left; i--){
+                ans.push_back(arr[bottom][i]);
+            }
                 bottom--;
             }
             
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
+            if(left<=right){
+                for(int i=bottom; i>=top; i--){
+                    ans.push_back(arr[i][left]);
                 }
                 left++;
+                
             }
+            
         }
-        
-        return result;
+        return ans;
     }
 };
