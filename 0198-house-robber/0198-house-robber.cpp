3465c1097735ac8e354ccc9368084b04c216a237
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-01 12:54:19 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (17.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,13 +13,18 @@ public:
         if(n==1) return nums[0];
         if(n==0) return 0;
        // return solve(n-1,nums,dp);
-        dp[0] = nums[0];
-        dp[1] = max(dp[0],nums[1]);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0],nums[1]);
+        int prev = nums[0];
+        int prev1 = max(nums[0],nums[1]);
         for(int i=2; i<n; i++){
-            int a = nums[i]+dp[i-2];
-            int b = dp[i-1];
-            dp[i] = max(a,b);
+            int a = nums[i]+prev;
+            int b = prev1;
+            int temp = max(a,b);
+            prev = prev1;
+            prev1 = temp;
+            
         }
-        return dp[n-1];
+        return prev1;
     }
 };
