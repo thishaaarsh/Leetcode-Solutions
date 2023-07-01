0081838e0bc8438ea157ab0b1e50afaa5687b7cd
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-01 16:54:11 +0530

    Time: 8 ms (56.43%), Space: 9 MB (22.19%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,7 +9,18 @@ public:
     }
     int minimumTotal(vector<vector<int>>& arr) {
         int n = arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(0,0,n,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0; i<n; i++){
+            dp[n-1][i] = arr[n-1][i];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                int up = arr[i][j]+dp[i+1][j];
+                int dia = arr[i][j]+dp[i+1][j+1];
+                dp[i][j] = min(up,dia);
+            }
+        }
+        return dp[0][0];
+        //return solve(0,0,n,arr,dp);
     }
 };
