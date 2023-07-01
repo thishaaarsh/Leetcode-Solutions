52235aac415b560bf490bc9037d23e098f8ed252
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-01 14:01:56 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& arr, int m, int n,vector<vector<int>>& dp){
-        if(m>=0 and n>=0 and arr[m][n] == 1){
-            return 0;
-        }
-        if(m==0 and n==0) return 1;
-        if(m<0 or n<0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
-        int up = solve(arr,m-1,n,dp);
-        int left = solve(arr,m,n-1,dp);
-        return dp[m][n] =  up+left;
+    int solve(int i,int j,vector<vector<int>>& arr,vector<vector<int>>&dp ){
+        if(i<0||j<0) return 0;
+        if(arr[i][j] == 1) return 0;
+        if(i==0 and j==0) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] =  solve(i-1,j,arr,dp)+solve(i,j-1,arr,dp);
+        
+        
     }
     int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int m = arr.size();
-        int n = arr[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(arr,m-1,n-1,dp);
+        int n = arr.size();
+        int m = arr[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(n-1,m-1,arr,dp);
     }
 };
