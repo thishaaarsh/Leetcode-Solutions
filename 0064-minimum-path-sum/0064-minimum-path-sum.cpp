fa36e034f3991d21bbf0db75a3ab00f0ff15d0a6
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-01 14:14:16 +0530

    Time: 12 ms (55.50%), Space: 10 MB (45.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -9,7 +9,21 @@ public:
     int minPathSum(vector<vector<int>>& arr) {
         int n = arr.size();
         int m = arr[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        //return solve(n-1,m-1,arr,dp);
+        dp[0][0] = arr[0][0];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 and j==0) continue;
+                int a = 1e9,b=1e9;
+                if(i>0){
+                    a = arr[i][j]+dp[i-1][j];
+                }
+                if(j>0)
+                    b = arr[i][j]+dp[i][j-1];
+                dp[i][j] = min(a,b);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
