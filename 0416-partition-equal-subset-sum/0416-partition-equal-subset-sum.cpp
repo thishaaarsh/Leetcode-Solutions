c80fea50f1797ed6a1f980659386ea615b023312
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-10 13:29:33 +0530

    Time: 332 ms (66.54%), Space: 95 MB (34.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    bool solve(int i, int target,vector<int>& arr,vector<vector<int>>& dp){
-        if(target == 0) return true;
-        if(i==0) return target == arr[0];
-        if(dp[i][target] != -1) return dp[i][target];
-        bool nottake = solve(i-1,target,arr,dp);
-        bool take =  false;
-        if(arr[i] <= target){
-            take = solve(i-1,target-arr[i],arr,dp);
+    bool solve(vector<int>& arr,int n, int k,vector<vector<int>>&dp){
+        if(k == 0) return true;
+        if(n<0) return false;
+        if(dp[n][k] != -1) return dp[n][k];
+        bool take = false;
+        if(arr[n] <= k){
+            take = solve(arr,n-1,k-arr[n],dp);
         }
-        return dp[i][target] =  take || nottake;
+        bool nottake = solve(arr,n-1,k,dp);
+        return dp[n][k] =  take || nottake;
     }
     bool canPartition(vector<int>& arr) {
-        int sum = 0;
         int n = arr.size();
-        for(int i=0; i<n; i++){
-            sum+=arr[i];
+        int sum = 0; 
+        for(auto x : arr){
+            sum+=x;
         }
         if(sum%2) return false;
-        vector<vector<int>>dp(n,vector<int>(sum/2+1,0));
-        //return solve(n-1,sum/2,arr,dp);
-        for(int i=0; i<n;i++){
-            for(int j=0; j<=sum/2;j++){
-                if(j==0){
-                    dp[i][j] = 1;
-                    continue;
-                }
-                if(i==0){
-                    if(arr[i] == j){
-                        dp[i][j] = 1;
-                    }
-                    continue;
-                }
-                int nottake = dp[i-1][j];
-                int take = 0;
-                if(arr[i] <= j){
-                    take = dp[i-1][j-arr[i]];
-                }
-                dp[i][j] = take || nottake;
-            }
-        }
-        return dp[n-1][sum/2];
+        vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
+        return solve(arr,n-1,sum/2,dp);
     }
 };
