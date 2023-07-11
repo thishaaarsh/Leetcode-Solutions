0965f42a49b746a2732de663c16f9417bd232faf
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-11 17:20:50 +0530

    Time: 686 ms (33.21%), Space: 292.2 MB (26.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,7 +12,18 @@ public:
     }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,n,arr,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        //return solve(0,-1,n,arr,dp);
+        for(int i=n-1; i>=0; i--){
+            for(int j=i-1; j>=-1; j--){
+                int nottake = dp[i+1][j+1];
+                int take = INT_MIN;
+                if(j==-1 || arr[j] < arr[i]){
+                    take = 1+dp[i+1][i+1];
+                }
+                dp[i][j+1] = max(take,nottake);
+            }
+        }
+        return dp[0][0];
     }
 };
