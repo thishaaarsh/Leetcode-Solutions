d3217ee11b7a3c6da61af4bf4ec92796b0ee9fbb
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-13 14:20:16 +0530

    Time: 72 ms (74.61%), Space: 22.3 MB (40.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& arr) {
         int n = arr.size();
-        int m = arr[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
-        int ans = 1;
-        q.push({0,0});
-        vis[0][0] = 1;
-        //int delrow[] = {-1,1,0,0};
-        //int delcol[] = {0,0,1,-1};
-        if(arr[0][0] == 1 || arr[n-1][m-1] == 1) return -1;
-        if(n==1 and m == 1) return 1;
+        if(n==0) return 0;
+        if(arr[0][0] == 1 || arr[n-1][n-1] == 1) return -1;
+        if(n==1) return 1;
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{0,0}});
+        vector<vector<int>>dist(n,vector<int>(n,1e9));
+        dist[0][0] = 1;
         while(!q.empty()){
-            int size = q.size();
-            ans++;
-            for(int k=0; k<size; k++){
-            int row = q.front().first;
-            int col = q.front().second;
+            auto node = q.front();
             q.pop();
-            
-            for(int j=-1; j<=1; j++){
-                for(int i=-1; i<=1; i++){
+            int dis = node.first;
+            int row = node.second.first;
+            int col = node.second.second;
+            for(int i=-1; i<=1; i++){
+                for(int j=-1; j<=1; j++){
                     int nrow = row+i;
                     int ncol = col+j;
-                    if(nrow == n-1 and ncol == m-1) return ans;
-                    if(nrow >=0 and ncol >=0 and nrow<n and ncol <m and 
-                        !vis[nrow][ncol] and arr[nrow][ncol] == 0){
-                            q.push({nrow,ncol});
-                            vis[nrow][ncol] = 1;
-                     }
+                    if(nrow >=0  and ncol >= 0  and nrow < n and ncol < n and 
+                       dis+1 < dist[nrow][ncol] and arr[nrow][ncol] == 0){
+                        dist[nrow][ncol] = dis+1;
+                        if(nrow == n-1 and ncol == n-1) return dist[nrow][ncol];
+                        q.push({dis+1,{nrow,ncol}});
+                    }
                 }
-            
             }
         }
-        
-        }
         return -1;
+        
     }
 };
