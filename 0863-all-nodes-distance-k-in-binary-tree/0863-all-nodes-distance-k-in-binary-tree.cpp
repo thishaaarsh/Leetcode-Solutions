e38bc7bc3e8a676a1c8406f57954f7b4f6259131
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-15 14:39:50 +0530

    Time: 7 ms (65.63%), Space: 13 MB (52.32%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,54 +9,56 @@
  */
 class Solution {
 public:
-    void markPer(TreeNode *root, unordered_map<TreeNode*, TreeNode*>&trackper){
+    void trackper(TreeNode* root,unordered_map<TreeNode *, TreeNode *> &mp){
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             TreeNode *curr = q.front();
             q.pop();
             if(curr->left){
-                trackper[curr->left] = curr;
+                mp[curr->left] = curr;
                 q.push(curr->left);
             }
-                
             if(curr->right){
-                trackper[curr->right] = curr;
+                mp[curr->right] = curr;
                 q.push(curr->right);
             }
         }
+        
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*>trackper;
-        markPer(root,trackper);
-        int currlev = 0;
+        vector<int>ans;
+        if(root == NULL) return ans;
+        unordered_map<TreeNode *, TreeNode *> mp;
+        trackper(root,mp);
+        unordered_map<TreeNode*,bool>vis;
         queue<TreeNode*>q;
         q.push(target);
-        unordered_map<TreeNode*,bool>vis;
         vis[target] = true;
+        int dis = 0;
         while(q.empty() == false){
+            if(dis == k) break;
+            dis++;
             int size = q.size();
-            if(currlev++ == k)
-                break;
             for(int i=0; i<size; i++){
-                TreeNode* curr = q.front();
+                TreeNode *node = q.front();
                 q.pop();
-                if(curr->left and !vis[curr->left]){
-                    q.push(curr->left);
-                    vis[curr->left] = true;
+                if(node->left and !vis[node->left]){
+                    q.push(node->left);
+                    vis[node->left] = true;
                 }
-                if(curr->right and !vis[curr->right]){
-                    q.push(curr->right);
-                    vis[curr->right] = true;
+                if(node->right and !vis[node->right]){
+                    q.push(node->right);
+                    vis[node->right] = true;
                 }
-                if(trackper[curr] and !vis[trackper[curr]]){
-                    q.push(trackper[curr]);
-                    vis[trackper[curr]] = true;
+                if(mp[node] and !vis[mp[node]]){
+                    q.push(mp[node]);
+                    vis[mp[node]] = true;
                 }
             }
+            
         }
-        vector<int>ans;
-        while(q.empty() == false){
+        while(!q.empty()){
             ans.push_back(q.front()->val);
             q.pop();
         }
