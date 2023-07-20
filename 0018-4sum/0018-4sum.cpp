37a7c4a0bf77ce799d3de608172a0a586f2dc958
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-20 19:29:59 +0530

    Time: 114 ms (43.45%), Space: 13.1 MB (72.49%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        set<vector<int>> st;
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        vector<vector<int>>ans;
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                unordered_set<long long>ss;
-                for(int k=j+1; k<n; k++){
-                    long long sum = (long long)nums[i]+(long long)nums[j];
-                    sum+=nums[k];
-                    long long last = (long long)(target-sum);
-                    if(ss.find(last) != ss.end()){
-                        vector<int>temp = {nums[i],nums[j],nums[k],(int)last};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                        
+            if(i==0 || arr[i] != arr[i-1]){
+                for(int j=i+1; j<n; j++){
+                    if(j==i+1 || arr[j] != arr[j-1]){
+                        int lo = j+1, hi = n-1;
+                       // long long sum = 
+                        while(lo<hi){
+                            long long sum = 1ll*arr[lo]+ 1ll*arr[hi]+1ll*arr[i]+1ll*arr[j];;
+                            if(sum==target){
+                                vector<int>temp = {arr[i],arr[j],arr[lo],arr[hi]};
+                                ans.push_back(temp);
+                                while(lo<hi and arr[lo]==arr[lo+1]) lo++;
+                                while(lo<hi and arr[hi] == arr[hi-1]) hi--;
+                                lo++;
+                                hi--;
+                            }
+                            else if(sum>target){
+                                hi--;
+                            }
+                            else lo++;
+                        }
                     }
-                    ss.insert(nums[k]);
                 }
             }
         }
-        vector<vector<int>>ans(st.begin(),st.end());
         return ans;
     }
 };
