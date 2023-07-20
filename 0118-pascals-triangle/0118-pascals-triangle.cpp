16686f964279790e970f7e188575727b83c075ea
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-07-20 19:18:29 +0530

    Time: 3 ms (40.91%), Space: 6.7 MB (36.14%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<int> solve(int n){
-        vector<int>temp;
-        int res = 1;
-        temp.push_back(res);
-        for(int i=1; i<n; i++){
-            res = res*(n-i);
-            res = res/i;
-            temp.push_back(res);
+    vector<int> solve(int i){
+        vector<int>ans;
+        ans.push_back(1);
+        for(int j=1; j<i; j++){
+            int res = ans.back()*(i-j);
+            res = res/j;
+            ans.push_back(res);
         }
-        return temp;
+        return ans;
     }
     vector<vector<int>> generate(int n) {
         vector<vector<int>>ans;
         for(int i=1; i<=n; i++){
-            vector<int>temp = solve(i);
-            ans.push_back(temp);
+            vector<int>a = solve(i);
+            ans.push_back(a);
         }
         return ans;
+        
     }
 };
