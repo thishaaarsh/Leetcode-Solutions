e68040992b9bb893728e23a73f7753bd67e30040
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-08-12 10:10:49 +0530

    Time: 7 ms (85.67%), Space: 20.2 MB (37.63%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -17,7 +17,7 @@ public:
         return dia;
     }
     int solve(TreeNode *root, int &dia){
-        if(root == NULL) return 0;
+        if(root  == NULL) return 0;
         int lh = solve(root->left,dia);
         int rh = solve(root->right,dia);
         dia = max(dia,lh+rh);
