e0dd214fd8a6dfb61fb48e0d7c1b89a23ff78714
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-10-30 19:02:23 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (55.26%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        int peak = l;
+
+        l = 0, r = peak;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int middle = mountainArr.get(mid);
+            if (middle == target) {
+                return mid;
+            } else if (middle < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        l = peak + 1;
+        r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int middle = mountainArr.get(mid);
+            if (middle == target) {
+                return mid;
+            } else if (middle < target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
