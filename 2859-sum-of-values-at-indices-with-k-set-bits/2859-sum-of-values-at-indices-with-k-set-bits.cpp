194bda02b3be1e076b17134ed3bd23ecda8526e8
thishaaarsh <90460581+thishaaarsh@users.noreply.github.com>
2023-12-26 20:11:03 +0530

    Time: 19 ms (6.45%), Space: 21.9 MB (45.14%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+   //  001100 => 000110 => 000011 => 000001 => 000000
+    int solve(int num){
+        int a = 0;
+        while(num){
+            if(num & 1 == 1) a++;
+            num = num >> 1;
+        }
+        return a;
+    }
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
         int ans = 0;
         for(int i=0; i<nums.size();i++){
-            int a = __builtin_popcount(i);
-           
+            int a = solve(i);
             if(a==k) ans+=nums[i];
         }
         return ans;
